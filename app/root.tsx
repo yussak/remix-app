4903f9fe517ddf2068289f3f5804cd09970f02a8
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-13 00:03:20 +0900

    read style

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,11 @@
+import type { LinksFunction } from "@remix-run/node";
 import { Links, LiveReload, Outlet } from "@remix-run/react";
+
+import globalStylesUrl from "~/styles/global.css"
+
+export const links: LinksFunction = () => [
+    { rel: "stylesheet", href: globalStylesUrl },
+]
 
 export default function App() {
   return (
