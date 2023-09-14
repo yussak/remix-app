c598eeeb0686d3b95a2d66661d7789c4d4b6f44b
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-14 21:04:58 +0900

    get joke detail

--- app/routes/jokes.$JokeId.tsx
+++ app/routes/jokes.$JokeId.tsx
@@ -1,11 +1,25 @@
+import { LoaderArgs, json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.JokeId },
+  });
+  if (!joke) {
+    throw new Error("Joke not found");
+  }
+  return json({ joke });
+};
+
 export default function Joke() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div>
       <p>here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} permalink</Link>
     </div>
-  )
+  );
 }
