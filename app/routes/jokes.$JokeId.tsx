ae10878ac5fec7505c36bbb2c2becadecda0cbff
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-14 21:05:33 +0900

    rename file

--- app/routes/jokes.$JokeId.tsx
+++ app/routes/jokes.$JokeId.tsx
@@ -4,7 +4,7 @@ import { db } from "~/utils/db.server";
 
 export const loader = async ({ params }: LoaderArgs) => {
   const joke = await db.joke.findUnique({
-    where: { id: params.JokeId },
+    where: { id: params.jokeId },
   });
   if (!joke) {
     throw new Error("Joke not found");
