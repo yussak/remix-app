86f06306c527e6ddd15c233697995d3f7aab54c5
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-14 23:43:55 +0900

    fix value

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -1,3 +1,21 @@
+import { type ActionArgs, redirect } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const content = form.get("content");
+  const name = form.get("name");
+
+  if (typeof content !== "string" || typeof name !== "string") {
+    throw new Error("form not submitted correctly.");
+  }
+
+  const fields = { content, name };
+
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJoke() {
   return (
     <div>
@@ -10,7 +28,7 @@ export default function NewJoke() {
         </div>
         <div>
           <label>
-            Content: <textarea name="name" />
+            Content: <textarea name="content" />
           </label>
         </div>
         <div>
@@ -18,5 +36,5 @@ export default function NewJoke() {
         </div>
       </form>
     </div>
-  )
+  );
 }
