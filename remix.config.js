e1351b17dd2d2e9335e26a29e0e13504d9422c79
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-29 17:41:38 +0900

    perf: add setting of not changing files

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,5 @@
+const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
@@ -13,5 +15,9 @@ module.exports = {
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
   },
 };
