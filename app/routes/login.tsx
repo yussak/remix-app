7442cacad6ac3ca944e3fe08704572c9c7a615c2
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-29 22:31:22 +0900

    feat: update login form

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,20 +1,62 @@
+import type { LinksFunction } from "@remix-run/node";
+import { Link, useSearchParams } from "@remix-run/react";
+
+import stylesUrl from "~/styles/login.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
+
 export default function Login() {
+  const [searchParams] = useSearchParams();
   return (
-    <form method="post">
-      <div className="container">
-        <div className="content">
-          <h1>login</h1>
+    <div className="container">
+      <div className="content" data-light="">
+        <h1>Login</h1>
+        <form method="post">
+          <input
+            type="hidden"
+            name="redirectTo"
+            value={searchParams.get("redirectTo") ?? undefined}
+          />
+          <fieldset>
+            <legend className="sr-only">Login or Register?</legend>
+            <label>
+              <input
+                type="radio"
+                name="loginType"
+                value="login"
+                defaultChecked
+              />{" "}
+              Login
+            </label>
+            <label>
+              <input type="radio" name="loginType" value="register" /> Register
+            </label>
+          </fieldset>
           <div>
-            <label>username</label>
-            <input type="text" name="username" />
+            <label htmlFor="username-input">Username</label>
+            <input type="text" id="username-input" name="username" />
           </div>
           <div>
-            <label>password</label>
-            <input type="password" name="password" />
+            <label htmlFor="password-input">Password</label>
+            <input id="password-input" name="password" type="password" />
           </div>
-          <button type="submit">submit</button>
-        </div>
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </form>
       </div>
-    </form>
+      <div className="links">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/jokes">Jokes</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
