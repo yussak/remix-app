99777abec0dda5429f4e3b85bf7c5e74b698b879
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-29 17:55:06 +0900

    perf: read assetsBuildDirectory

--- remix.config.js
+++ remix.config.js
@@ -4,7 +4,7 @@ const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
+  assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   serverModuleFormat: "cjs",
