3ed3180e0f029846d970b1f12320f5ab590c8985
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-30 21:37:15 +0900

    feat: update data to use faker

--- app/test/jokes/get_user.test.ts
+++ app/test/jokes/get_user.test.ts
@@ -1,24 +1,24 @@
 import { expect, test } from "vitest";
 // TODO:@/のように読みたい
 import { db } from "../../utils/db.server";
+import { faker } from "@faker-js/faker";
 
 test("can get user", async () => {
   // TODO:faker-jsで書き換える
   //　https://fakerjs.dev/guide/frameworks.html
   const mockUser = {
-    id: "e2872ed6-6b9e-4d88-82bf-0f21734ba555",
-    createdAt: "2023-09-30T05:37:31.109Z",
-    updatedAt: "2023-09-30T05:37:31.109Z",
-    username: "user5",
-    passwordHash:
-      "$2a$10$.IAF/11dLE9wpmMy1GaejeoxLfMjkdyjHsOCZw07HGxrpAlERXiT6",
+    id: faker.string.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    username: faker.person.fullName(),
+    passwordHash: faker.internet.password(),
   };
-  // console.log("mockUserは", mockUser);
+  console.log("mockUserは", mockUser);
 
   const result = await db.user.findUnique({
-    where: { id: "e2872ed6-6b9e-4d88-82bf-0f21734ba555" },
+    where: { id: mockUser.id },
   });
-  // console.log("resultは", result);
+  console.log("resultは", result);
 
   // 'result' は 'null' の可能性があります。とあるので必要なら対処
   expect(mockUser.id).toEqual(result.id);
