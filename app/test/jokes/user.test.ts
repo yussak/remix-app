81db8dafc8bff412c3c5f08c610e29b98a906c6b
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-10-01 00:18:26 +0900

    docs: fix comment

--- app/test/jokes/user.test.ts
+++ app/test/jokes/user.test.ts
@@ -3,10 +3,12 @@ import { expect, test } from "vitest";
 import { db } from "../../utils/db.server";
 import { faker } from "@faker-js/faker";
 
-// このファイルでuser CRUDまとめてやるかもしれない
+// このファイルでuser CRUDまとめてやる
 // とりあえず通るテストを書く。リファクタリングは後
 
 test("can get user", async () => {
+  // テストのたびにuser作られてしまうのでテスト後に削除する
+  // db.user.delete実装後テストコードの最後で呼び出す
   const createUser = await db.user.create({
     data: {
       username: faker.person.fullName(),
