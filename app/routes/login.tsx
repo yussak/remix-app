061868d5e3e5dc3f175fa33d4ee85f0a9f7066f2
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-30 11:06:15 +0900

    feat: enable to login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,6 +4,7 @@ import { Link, useActionData, useSearchParams } from "@remix-run/react";
 import stylesUrl from "~/styles/login.css";
 import { db } from "~/utils/db.server";
 import { badRequest } from "~/utils/request.server";
+import { login } from "~/utils/session.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
@@ -64,9 +65,15 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
   switch (loginType) {
     case "login": {
-      // login to get the user
-      // if there's no user, return the fields and a formError
-      // if there is a user, create their session and redirect to /jokes
+      const user = await login({ username, password });
+      console.log({ user });
+      if (!user) {
+        return badRequest({
+          fieldErrors: null,
+          fields,
+          formError: "Username/Password combination is incorrect",
+        });
+      }
       return badRequest({
         fieldErrors: null,
         fields,
---
+++ app/utils/session.server.ts
@@ -0,0 +1,24 @@
+import bcrypt from "bcryptjs";
+
+import { db } from "./db.server";
+
+type LoginForm = {
+  password: string;
+  username: string;
+};
+
+export async function login({ password, username }: LoginForm) {
+  const user = await db.user.findUnique({
+    where: { username },
+  });
+  if (!user) {
+    return null;
+  }
+
+  const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
+  if (!isCorrectPassword) {
+    return null;
+  }
+
+  return { id: user.id, username };
+}
