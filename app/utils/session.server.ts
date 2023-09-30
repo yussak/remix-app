0346f6610e2da4fed171d83e9995d7185c7d1315
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-30 11:25:49 +0900

    feat: enable to create session

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 
 import { db } from "./db.server";
+import { createCookieSessionStorage, redirect } from "@remix-run/node";
 
 type LoginForm = {
   password: string;
@@ -21,4 +22,34 @@ export async function login({ password, username }: LoginForm) {
   }
 
   return { id: user.id, username };
+}
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "RJ_session",
+    // normally you want this to be `secure: true`
+    // but that doesn't work on localhost for Safari
+    // https://web.dev/when-to-use-local-https/
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set("userId", userId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session),
+    },
+  });
 }
