552c7596f791f8cd88cc4296493490ac2be4c494
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-30 14:13:05 +0900

    feat: add register function

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -8,6 +8,14 @@ type LoginForm = {
   username: string;
 };
 
+export async function register({ password, username }: LoginForm) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = await db.user.create({
+    data: { passwordHash, username },
+  });
+  return { id: user.id, username };
+}
+
 export async function login({ password, username }: LoginForm) {
   const user = await db.user.findUnique({
     where: { username },
